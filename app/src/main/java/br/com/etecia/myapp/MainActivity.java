fc8fe1dedd7453e1ed6e661ee643fb9d0a8f7a98
rafaella-sbizarro <rafaellasbizarrop24@gmail.com>
2025-05-15 20:20:19 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -0,0 +1,49 @@
+package br.com.etecia.myapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    //declarando a variavel qu representara o componente xml
+    FloatingActionButton idFAB;
+    ExtendedFloatingActionButton idFabExtend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // apresentar o java ao xml
+        idFAB = findViewById(R.id.dFab);
+        idFabExtend = findViewById(R.id.idFabExtend);
+
+        idFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Cliquei no FAB", Toast.LENGTH_SHORT).show();
+            }
+        });
+        idFabExtend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Cliquei no FABExtend", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
